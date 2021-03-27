90d9d7da9b65cc73ea3500c574b82110061efcfd
Kent Braaten <kentbraaten@frontiernet.net>
2021-03-27 10:39:11 -0500

    put all I2C functions in the same module

--- I2CFuncs.py
+++ I2CFuncs.py
@@ -1,6 +1,18 @@
 from smbus2 import SMBus, i2c_msg
 import struct
 
+
+def turn_on_valve(address):
+    command = 1;
+    i2cPort, pin = address
+    bytesToSend = command.to_bytes(1,'big') + pin.to_bytes(4,'big')
+    with SMBus(1) as bus:
+        write = i2c_msg.write(i2cPort, bytesToSend)
+        bus.i2c_rdwr(write)
+        returnVal = 0x01 #read.buf[0]
+        return returnVal
+
+    
 def get_sensor_reading(sensorAddress):
     i2CAddress, pin = sensorAddress
     bytes_to_send = pin.to_bytes(4,'big')
@@ -13,11 +25,9 @@ def get_sensor_reading(sensorAddress):
         returnVal = bytes_to_int(read)
     return returnVal
 
+
 def bytes_to_int(msg):
     result = 0
     for b in msg:
         result = result * 256 + int(b)
     return result;
-
-address = (4,1)
-print(get_sensor_reading(address))
